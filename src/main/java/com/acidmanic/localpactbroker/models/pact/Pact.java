6dc1b1c8771dbf0cf9bb92497a04fddf78f809c1
Mani Moayedi <you@example.com>
2021-04-14 17:26:16 +0430

    Add Pact Storage

---
+++ src/main/java/com/acidmanic/localpactbroker/models/pact/Pact.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.localpactbroker.models.pact;
+
+import java.util.List;
+
+/**
+ *
+ * @author diego
+ */
+public class Pact {
+
+    private List<Contract> contracts;
+
+    public List<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<Contract> contracts) {
+        this.contracts = contracts;
+    }
+}
