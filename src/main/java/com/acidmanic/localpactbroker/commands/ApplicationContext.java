e7bad5eb8e1ce9de563622f3242bee12e4087c1e
Mani Moayedi <you@example.com>
2021-04-14 23:14:55 +0430

    Update ApplicationContext to carry storages

--- src/main/java/com/acidmanic/localpactbroker/commands/ApplicationContext.java
+++ src/main/java/com/acidmanic/localpactbroker/commands/ApplicationContext.java
@@ -6,13 +6,29 @@
 package com.acidmanic.localpactbroker.commands;
 
 import com.acidmanic.commandline.commands.context.ExecutionContext;
+import com.acidmanic.localpactbroker.storage.PactStorage;
+import com.acidmanic.localpactbroker.storage.TokenStorage;
 
 /**
  *
  * @author diego
  */
-public class ApplicationContext implements ExecutionContext{
-    
-    
-    
+public class ApplicationContext implements ExecutionContext {
+
+    private final TokenStorage tokenStorage;
+    private final PactStorage pactStorage;
+
+    public ApplicationContext(TokenStorage tokenStorage, PactStorage pactStorage) {
+        this.tokenStorage = tokenStorage;
+        this.pactStorage = pactStorage;
+    }
+
+    public TokenStorage getTokenStorage() {
+        return tokenStorage;
+    }
+
+    public PactStorage getPactStorage() {
+        return pactStorage;
+    }
+
 }
