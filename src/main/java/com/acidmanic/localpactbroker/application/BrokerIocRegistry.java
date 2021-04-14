1a12e0bdd5898d19e86139510a8da7f6a533064a
Mani Moayedi <you@example.com>
2021-04-14 23:22:52 +0430

    Register Dependencies

--- src/main/java/com/acidmanic/localpactbroker/application/BrokerIocRegistry.java
+++ src/main/java/com/acidmanic/localpactbroker/application/BrokerIocRegistry.java
@@ -5,36 +5,61 @@
  */
 package com.acidmanic.localpactbroker.application;
 
+import com.acidmanic.commandline.commands.TypeRegistery;
+import com.acidmanic.localpactbroker.application.services.ConsoleService;
+import com.acidmanic.localpactbroker.commands.Token;
+import com.acidmanic.localpactbroker.storage.PactStorage;
+import com.acidmanic.localpactbroker.storage.TokenStorage;
 import com.acidmanic.utility.myoccontainer.Installer;
 import com.acidmanic.utility.myoccontainer.Registerer;
+import com.acidmanic.utility.myoccontainer.Resolver;
+import com.acidmanic.utility.myoccontainer.lifetimemanagement.LifetimeType;
 
 /**
  *
  * @author diego
  */
-public class BrokerIocRegistry implements Installer{
+public class BrokerIocRegistry implements Installer {
 
     @Override
     public void configure(Registerer reg) {
-        
+
         configureApplicationServices(reg);
-        
+
         configureInfrastructureServices(reg);
-        
+
         configureControllers(reg);
-        
+
     }
 
     private void configureApplicationServices(Registerer reg) {
-        
+        reg.register().bindToSelf(ConsoleService.class).livesAsA(LifetimeType.Singleton);
     }
 
     private void configureInfrastructureServices(Registerer reg) {
-        
+
+        TypeRegistery commandsRegistery = new TypeRegistery();
+
+        reg.register().bind(TypeRegistery.class).withBuilder(() -> commandsRegistery)
+                .livesAsA(LifetimeType.Singleton);
+
+        configureCommands(commandsRegistery);
+
+        reg.register().bind(Resolver.class).withBuilder(() -> BrokerResolver.makeInstance());
+
+        reg.register().bindToSelf(TokenStorage.class).livesAsA(LifetimeType.Transient);
+
+        reg.register().bindToSelf(PactStorage.class).livesAsA(LifetimeType.Transient);
+
     }
 
     private void configureControllers(Registerer reg) {
-        
+
     }
-    
+
+    private void configureCommands(TypeRegistery reg) {
+
+        reg.registerClass(Token.class);
+    }
+
 }
