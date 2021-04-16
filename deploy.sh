f8d477503a1ad0463605f8ced5fdcd810ea70d9a
Mani Moayedi <you@example.com>
2021-04-17 00:41:59 +0430

    fix qoutes for deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 
 BASE=target/broker-server
 
+rm -rf $BASE
+
 mvn clean install && \
 mkdir $BASE && \
 cp target/application-standalone.jar $BASE && \
-echo 'java -jar application-standalone.jar $@ &' > $BASE/start.sh && \
+echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/start.sh && \
+echo 'DIR=$(realpath $DIR)' >> $BASE/start.sh && \
+echo 'JAR=$DIR/application-standalone.jar' >> $BASE/start.sh && \
+echo 'java -jar "$JAR" $@ &' >> $BASE/start.sh && \
 chmod +x $BASE/start.sh && \
 echo 'touch .kill' > $BASE/stop.sh && \
-chmod +x $BASE/stop.sh
-
+chmod +x $BASE/stop.sh && \
+echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/token.sh && \
+echo 'DIR=$(realpath $DIR)' >> $BASE/token.sh && \
+echo 'JAR=$DIR/application-standalone.jar' >> $BASE/token.sh && \
+echo 'java -jar "$JAR" token' >> $BASE/token.sh && \
+chmod +x $BASE/token.sh
