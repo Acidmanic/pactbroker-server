8ef5d59ec751988b17abec5bb581cd7107d8f1a1
Mani Moayedi <you@example.com>
2021-05-04 23:22:13 +0430

    Change deployment due to maven-assembly+jboss-bodywriters issue

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,18 @@ rm -rf $BASE
 
 mvn clean install && \
 mkdir $BASE && \
-cp target/application-standalone.jar $BASE && \
+cp target/*.jar $BASE/application.jar && \
+mkdir $BASE/dependency && \
+cp -R target/dependency/* $BASE/dependency/ && \
 echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/start.sh && \
 echo 'DIR=$(realpath $DIR)' >> $BASE/start.sh && \
-echo 'JAR=$DIR/application-standalone.jar' >> $BASE/start.sh && \
+echo 'JAR=$DIR/application.jar' >> $BASE/start.sh && \
 echo 'java -jar "$JAR" $@ &' >> $BASE/start.sh && \
 chmod +x $BASE/start.sh && \
 echo 'touch .kill' > $BASE/stop.sh && \
 chmod +x $BASE/stop.sh && \
 echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/token.sh && \
 echo 'DIR=$(realpath $DIR)' >> $BASE/token.sh && \
-echo 'JAR=$DIR/application-standalone.jar' >> $BASE/token.sh && \
+echo 'JAR=$DIR/application.jar' >> $BASE/token.sh && \
 echo 'java -jar "$JAR" token' >> $BASE/token.sh && \
 chmod +x $BASE/token.sh
