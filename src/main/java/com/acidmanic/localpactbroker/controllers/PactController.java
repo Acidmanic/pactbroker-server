894398947fdf443b73da20381a3292e8693f7838
Mani Moayedi <acidmanic.moayedi@gmail.com>
2021-07-07 13:19:15 +0430

    Update PactsController to use manager

--- src/main/java/com/acidmanic/localpactbroker/controllers/PactController.java
+++ src/main/java/com/acidmanic/localpactbroker/controllers/PactController.java
@@ -9,14 +9,21 @@ import com.acidmanic.delegates.Function;
 import com.acidmanic.lightweight.logger.Logger;
 import com.acidmanic.localpactbroker.application.services.web.Controller;
 import com.acidmanic.localpactbroker.models.Dto;
+import com.acidmanic.localpactbroker.models.PactMap;
+import com.acidmanic.localpactbroker.services.PactsManagerService;
+import com.acidmanic.localpactbroker.storage.PactMapStorage;
 import com.acidmanic.localpactbroker.storage.PactStorage;
 import com.acidmanic.localpactbroker.storage.TokenStorage;
 import com.acidmanic.localpactbroker.utility.Result;
 import com.acidmanic.pact.models.Pact;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -27,11 +34,11 @@ import javax.ws.rs.Produces;
 @Path("/")
 public class PactController extends ControllerBase {
 
-    private final PactStorage pactStorage;
+    private final PactsManagerService pactsManagerService;
 
-    public PactController(PactStorage pactStorage, TokenStorage tokenStorage, Logger logger) {
+    public PactController(PactsManagerService pactsManagerService, TokenStorage tokenStorage, Logger logger) {
         super(tokenStorage, logger);
-        this.pactStorage = pactStorage;
+        this.pactsManagerService = pactsManagerService;
     }
 
     @GET
@@ -41,13 +48,7 @@ public class PactController extends ControllerBase {
 
         Function<Dto<Pact>> innerCode = () -> {
 
-            Result<Pact> pactResult = this.pactStorage.read();
-
-            if (pactResult.isSuccessfull()) {
-                return new Dto(pactResult.getValue());
-            } else {
-                return Models.failureDto(HttpStatus.NOT_FOUND);
-            }
+            return this.pactsManagerService.pull();
         };
 
         Dto<Pact> response = authorize(token, innerCode);
@@ -68,23 +69,67 @@ public class PactController extends ControllerBase {
 
         Function<Dto<String>> innerCode = () -> {
 
-            if (pact == null) {
-
-                return Models.failureDto(HttpStatus.BAD_REQUEST);
-            }
-            boolean success = this.pactStorage.write(pact);
-
-            if (success) {
-
-                return Models.SuccessDto();
-            } else {
-
-                return Models.failureDto(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            return this.pactsManagerService.push(pact);
 
         };
 
         Dto<String> response = authorize(token, innerCode);
+
+        return response;
+    }
+
+    /**
+     *
+     * @param token
+     * @param tag
+     * @param pact
+     * @return
+     */
+    @POST
+    @Path("/store/{tag}")
+    @Produces("application/json")
+    public Dto<String> store(@HeaderParam("token") String token,
+            @PathParam("tag") String tag,
+            Pact pact) {
+
+        Function<Dto<String>> innerCode = () -> {
+
+            return this.pactsManagerService.store(tag, pact);
+        };
+
+        Dto<String> response = authorize(token, innerCode);
+
+        return response;
+    }
+
+    @GET
+    @Path("/tags")
+    @Produces("application/json")
+    public Dto<List<String>> getTags(@HeaderParam("token") String token) {
+
+        Function<Dto<List<String>>> innerCode = () -> {
+
+            return pactsManagerService.getTags();
+        };
+
+        Dto<List<String>> response = authorize(token, innerCode);
+
+        return response;
+    }
+
+    @PUT
+    @Path("/elect/{tag}")
+    @Produces("application/json")
+    public Dto<Pact> elect(
+            @HeaderParam("token") String token,
+            @PathParam("tag") String tag) {
+
+        Function<Dto<Pact>> innerCode = () -> {
+
+            return pactsManagerService.elect(tag);
+        };
+
+        Dto<Pact> response = authorize(token, innerCode);
 
         return response;
     }
