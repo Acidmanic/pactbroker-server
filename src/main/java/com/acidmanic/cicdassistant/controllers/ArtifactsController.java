7fa07ec6d1d1d50d3c0dc87db9827795f6f092d5
Mani Moayedi <acidmanic.moayedi@gmail.com>
2021-07-21 02:53:13 +0430

    Make it authorize for uploading artifacts

--- src/main/java/com/acidmanic/cicdassistant/controllers/ArtifactsController.java
+++ src/main/java/com/acidmanic/cicdassistant/controllers/ArtifactsController.java
@@ -7,34 +7,22 @@ package com.acidmanic.cicdassistant.controllers;
 
 import com.acidmanic.cicdassistant.controllers.models.FileUploadForm;
 import com.acidmanic.cicdassistant.application.services.web.Controller;
+import com.acidmanic.cicdassistant.models.Dto;
 import com.acidmanic.cicdassistant.services.ArtifactManager;
 import com.acidmanic.cicdassistant.services.MultipartFileSender;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.Buffer;
-import java.util.List;
-import java.util.Map;
+import com.acidmanic.cicdassistant.storage.TokenStorage;
+import com.acidmanic.delegates.Function;
+import com.acidmanic.lightweight.logger.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import org.apache.commons.io.IOUtils;
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 /**
@@ -43,11 +31,12 @@ import org.jboss.resteasy.spi.ResteasyProviderFactory;
  */
 @Path("artifacts")
 @Controller
-public class ArtifactsController {
+public class ArtifactsController extends ControllerBase {
 
     private final ArtifactManager artifactManager;
 
-    public ArtifactsController(ArtifactManager artifactManager) {
+    public ArtifactsController(ArtifactManager artifactManager, TokenStorage tokenStorage, Logger logger) {
+        super(tokenStorage, logger);
         this.artifactManager = artifactManager;
     }
 
@@ -81,16 +70,33 @@ public class ArtifactsController {
     @POST
     @Path("/upload")
     @Consumes("multipart/form-data")
-    public Response uploadFile(@MultipartForm FileUploadForm form) {
+    public Response uploadFile(
+            @MultipartForm FileUploadForm form,
+            @HeaderParam("token") String token) {
 
-        String fileName = form.getFileName() == null ? "Unknown" : form.getFileName();
+        Function<Dto<Response>> code = () -> {
 
-        this.artifactManager.writeArtifact(fileName, form.getFileData());
+            String fileName = form.getFileName() == null ? "Unknown" : form.getFileName();
 
-        return Response.status(200)
-                .entity("uploadFile is called, Uploaded file name: "
-                        + fileName + "\n")
-                .build();
+            this.artifactManager.writeArtifact(fileName, form.getFileData());
+
+            Response response = Response.status(200)
+                    .entity("uploadFile is called, Uploaded file name: "
+                            + fileName + "\n")
+                    .build();
+
+            return new Dto<>(response);
+        };
+
+        Dto<Response> authorizedResult = authorize(token, code);
+
+        if (authorizedResult.isFailure()) {
+
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        } else {
+            
+            return authorizedResult.getModel();
+        }
     }
 
 }
