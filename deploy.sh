d4269f65e7246b938e9d961d068dfa4b5e1ee01b
Mani Moayedi <acidmanic.moayedi@gmail.com>
2021-08-13 18:08:38 +0430

    Setup buitins assets

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,8 @@ mkdir $BASE/dependency && \
 cp -R target/dependency/* $BASE/dependency/ && \
 mkdir $BASE/wiki-styles && \
 cp -R target/wiki-styles/* $BASE/wiki-styles/ && \
+mkdir $BASE/assets && \
+cp -R target/assets $BASE/ && \
 echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/start.sh && \
 echo 'DIR=$(realpath $DIR)' >> $BASE/start.sh && \
 echo 'JAR=$DIR/application.jar' >> $BASE/start.sh && \
@@ -33,5 +35,5 @@ echo 'DIR=$(realpath $DIR)' >> $BASE/token.sh && \
 echo 'JAR=$DIR/application.jar' >> $BASE/token.sh && \
 echo 'java -jar "$JAR" token' >> $BASE/token.sh && \
 chmod +x $BASE/token.sh
-rm -rf $DIST_DIR/cicd-assistant.zip && (cd $DIST_DIR/cicd-assistant &&  zip ../cicd-assistant.zip **/* *)
+rm -rf $DIST_DIR/cicd-assistant.zip && (cd $DIST_DIR/cicd-assistant &&  zip -r ../cicd-assistant.zip **/* *)
 
